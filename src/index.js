fc27cf7bac356fb596b45fc98cde3b0ad5c110a2
Agie Fadillah <arigierf@gmail.com>
2024-01-14 15:52:24 +0700

    useState Hook

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './components/App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App2';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
